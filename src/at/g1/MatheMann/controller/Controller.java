379432d146dd2c1be80105af27f1fade030112b0
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-05-19 10:32:23 +0200

    Implemented load function for main program

--- src/at/g1/MatheMann/controller/Controller.java
+++ src/at/g1/MatheMann/controller/Controller.java
@@ -1,4 +1,51 @@
 package at.g1.MatheMann.controller;
 
-public class Controller {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ * MatheMann - Controller
+ * @author Niklas Schachl
+ * @version: 1.0, 19.5.2021
+ */
+public class Controller
+{
+    private Stage stage;
+
+    public Controller()
+    {
+
+    }
+
+    public static void show(Stage stage)
+    {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(Controller.class.getResource("/at/g1/MatheMann/view/view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            //get controller which is connected to this fxml file
+            Controller ctrl = fxmlLoader.getController();
+            ctrl.stage = stage;
+
+            stage.setTitle("MatheMann");
+            //stage.getIcons().add(new Image("/at/nsc/images/icon_logo.png"));
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch (Exception exception)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Internal Error");
+            alert.setContentText(String.format("An internal Error occurred. Please restart the program%nor contact the developer on GitHub%n%nError message: %s", exception.getMessage()));
+            alert.setResizable(true);
+            alert.showAndWait();
+            System.err.println(exception.getMessage());
+            exception.printStackTrace(System.err);
+        }
+    }
 }
--- src/at/g1/MatheMann/main/Main.java
+++ src/at/g1/MatheMann/main/Main.java
@@ -1,7 +1,24 @@
 package at.g1.MatheMann.main;
 
-public class Main {
-    public static void main(String[] args) {
+import at.g1.MatheMann.controller.Controller;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+/**
+ * MatheMann - Main
+ * @author Niklas Schachl
+ * @version 1.0, 19.5.2021
+ */
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Controller.show(stage);
     }
 }
