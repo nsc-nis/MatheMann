ef79162fea64f46c16e0352b59b56f9045d322bf
sperrsim <simon.sperr@htl-wels.at>
2021-05-19 10:27:00 +0200

    Added Documentation

--- src/at/g1/MatheMann/model/Question.java
+++ src/at/g1/MatheMann/model/Question.java
@@ -4,6 +4,14 @@ public class Question {
     private String value;
     private Answer[] answers = new Answer[4];
 
+    /**
+     * Creates a Question with 4 possible Answers
+     * @param value sets the Text of the Question
+     * @param answer1 first possible Answer to the Question
+     * @param answer2 second possible Answer to the Question
+     * @param answer3 third possible Answer to the Question
+     * @param answer4 fourth possible Answer to the Question
+     */
     public Question (String value, Answer answer1, Answer answer2, Answer answer3, Answer answer4)
     {
         this.value = value;
@@ -13,26 +21,46 @@ public class Question {
         this.answers[3] = answer4;
     }
 
+    /**
+     * sets the Text of the Question
+     * @param value Question text
+     */
     public void setValue(String value) {
         this.value = value;
     }
 
-    public void addAnswer(Answer answer)
+    /**
+     * Tries to add an Answer to the Question
+     * @param answer The Answer to add to the Question
+     * @return True if the Answer could be added, False if the Question already has its 4 Answers
+     */
+    public boolean addAnswer(Answer answer)
     {
+        boolean wasAdded = false;
         for(int i = 0; i < this.answers.length; i++)
         {
             if(this.answers[i] == null)
             {
                 this.answers[i] = answer;
+                wasAdded = true;
                 break;
             }
         }
+        return wasAdded;
     }
 
+    /**
+     * Returns the Question text
+     * @return Question text
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Gives all possible answers to the Question
+     * @return an Array of 4 answers
+     */
     public Answer[] getAnswers() {
         return answers;
     }
