1797bf197ca9d05d3685b85284c5186e8b36a577
Niklas Schachl <niklas.schachl@htl-wels.at>
2021-06-25 16:17:46 +0200

    Bugfixes

--- src/at/g1/MatheMann/controller/mainmenu/MainMenuC.java
+++ src/at/g1/MatheMann/controller/mainmenu/MainMenuC.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
